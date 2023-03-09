8ed479a5ff73d119a8a80f458d77cd34a8cb6276
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-03-09 19:59:59 +0200

    added pack4 that has been accidentally deleted and fixed the css not rendering on loose.js Deleted pack 3, and pack6 too big of an file to upload github

--- frontend/src/components/Loose.js
+++ frontend/src/components/Loose.js
@@ -2,18 +2,18 @@ import React from 'react'
 import './WinLoss.css'
 
 
-function Lose() {
+function Loose() {
 
   const handleGoBack = () => {
     window.location.href = "/";
   };
 
   return (
-    <div className='wordleLose'>
+    <div className='wordleLoose'>
       <h1>You Lost</h1>
       <button className='go-back-btn' onClick={handleGoBack}>Go back</button>
     </div>
   )
 }
 
-export default Lose
+export default Loose
